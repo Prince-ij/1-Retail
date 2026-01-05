36c3fc11d78c2a08c4bad20a6a176b55d2164e5d
Prince_ij <aminuibrahimthefifth@gmail.com>
2026-01-05 06:53:15 +0100

    feat: NavBar Complete

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -2,7 +2,18 @@ import { Button } from "react-bootstrap";
 import { useAppDispatch } from "../../hooks";
 import { useNavigate } from "react-router-dom";
 import { logOut } from "../../reducers/userReducer";
-import { LogOut } from "lucide-react";
+import '../../index.css'
+import {
+  CreditCard,
+  LayoutDashboard,
+  LogOut,
+  Package,
+  ShoppingCart,
+} from "lucide-react";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const NavBar = () => {
   const dispatch = useAppDispatch();
@@ -14,10 +25,58 @@ const NavBar = () => {
   };
   return (
     <>
-      <Button variant="danger" onClick={handleLogOut}>
-        <LogOut />
-        Log Out
-      </Button>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/" className="eagle text-secondary">
+            1-Retail
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0 ">
+              <Nav.Link
+                as={Link}
+                to="/"
+                className="d-flex align-items-center gap-2 nav-action"
+              >
+                <LayoutDashboard size={18} />
+                Dashboard
+              </Nav.Link>
+
+              <Nav.Link
+                as={Link}
+                to="/sales"
+                className="d-flex align-items-center gap-2 nav-action"
+              >
+                <ShoppingCart size={18} />
+                Sales
+              </Nav.Link>
+
+              <Nav.Link
+                as={Link}
+                to="/products"
+                className="d-flex align-items-center gap-2 nav-action"
+              >
+                <Package size={18} />
+                Product
+              </Nav.Link>
+
+              <Nav.Link
+                as={Link}
+                to="/credits"
+                className="d-flex align-items-center gap-2 nav-action"
+              >
+                <CreditCard size={18} />
+                Debts
+              </Nav.Link>
+            </Nav>
+            <Button variant="secondary" onClick={handleLogOut}>
+              <LogOut />
+              Log Out
+            </Button>
+
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
